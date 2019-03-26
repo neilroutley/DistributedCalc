4299a54ef8a8b2b9c6603cb3d340ed5b9ea848ab
neilroutley <lenderachu@gmail.com>
2019-03-26 14:15:12 -0700

    fixed addition bug in MainActivity.kt; added comments in same file and gradle files.

--- app/src/main/java/com/androidatc/kotlin/distributedcalc/MainActivity.kt
+++ app/src/main/java/com/androidatc/kotlin/distributedcalc/MainActivity.kt
@@ -18,21 +18,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var url : String? = null
+
+    // get the Firebase db instance
     private val database = FirebaseDatabase.getInstance()
+
+    // get a reference in the db for the apiURL
     private val myAPIRef = database.getReference("apiUrl")
 
     override fun onStart() {
         super.onStart()
 
+        // create an event listener on the db reference to detect changes
         myAPIRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 val value = dataSnapshot.getValue(String::class.java)
                 if (value != null) {
+                    // set the url value for the api endpoint
                     url = value
-                    Toast.makeText(this@MainActivity, value, Toast.LENGTH_SHORT).show()
                 } else {
+                    // problem getting api endpoint url from firebase
                     Toast.makeText(this@MainActivity, "No/bad val: ".plus(value), Toast.LENGTH_SHORT).show()
                 }
                 Log.d(TAG, "Value is: $value")
@@ -70,11 +76,13 @@ class MainActivity : AppCompatActivity() {
         tvOpen.setOnClickListener { appendOnExp("(", false) }
         tvClose.setOnClickListener { appendOnExp(")", false) }
 
+        // Clear button listener
         tvClear.setOnClickListener{
             tvExpression.text = ""
             tvResult.text = ""
         }
 
+        // Back button listener
         tvBack.setOnClickListener {
             var string = tvExpression.text.toString()
             if(string.isNotEmpty()){
@@ -83,19 +91,27 @@ class MainActivity : AppCompatActivity() {
             tvResult.text = ""
         }
 
+        // Equals button listener
+        // submits a request to the RESTapi with the expression in the calc value
         tvEquals.setOnClickListener {
             try {
                 val queue = Volley.newRequestQueue(this)
 
+                // get the raw expression from the textbox
+                val expression = tvExpression.text.toString()
+
+                // change + to %2B to be sent in request
+                val fixedExpression = expression.replace("+", "%2B")
+
                 // add the expression to the url before passing it to the RESTapi
-                val expressionUrl = url.plus(tvExpression.text.toString())
+                val expressionUrl = url.plus(fixedExpression)
 
                 // new json object get request and setting tvResult with the returned
                 // result from the RESTapi
                 val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, expressionUrl, null,
                     Response.Listener { response ->
+                        // set the result textbox to the returned value
                         tvResult.text = response.getString("result")
-                        Toast.makeText(this@MainActivity, "result: ".plus(response.getString("result")), Toast.LENGTH_SHORT).show()
                     },
                     Response.ErrorListener { error ->
                         Toast.makeText(this@MainActivity, "No/bad val: ".plus(error), Toast.LENGTH_SHORT).show()
@@ -110,6 +126,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // logic for when to clear the expression textfield
     fun appendOnExp(string : String, canClear: Boolean){
         if(tvResult.text.isNotEmpty()){
             tvResult.text = ""
